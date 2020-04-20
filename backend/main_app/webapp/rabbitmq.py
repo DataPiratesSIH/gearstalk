0f66dacd9d7440e18d2ebd3c9d6a3fdbe8ddc481
carol80 <carolseb80@gmail.com>
2020-04-20 23:31:03 +0530

    live_cams add

--- backend/main_app/webapp/rabbitmq.py
+++ backend/main_app/webapp/rabbitmq.py
@@ -1,14 +1,14 @@
 import pika
 import json
 
-def rabbitmq(string,filename):
+def rabbitmq_upload(string,filename):
     credentials = pika.PlainCredentials('test', 'test')
 
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='127.0.1.1', credentials=credentials))
     channel = connection.channel()
 
-    channel.queue_declare(queue='json')
+    channel.queue_declare(queue='uploading')
 
     data = {
         "file": filename,
@@ -16,6 +16,27 @@ def rabbitmq(string,filename):
     }
     message = json.dumps(data, ensure_ascii=False, indent=4)
     
-    channel.basic_publish(exchange='', routing_key='json', body=message)
+    channel.basic_publish(exchange='', routing_key='uploading', body=message)
+    print(" [x] Sent The JSON Data")
+    connection.close()
+
+
+def rabbitmq_live(domain,lat,lng):
+    credentials = pika.PlainCredentials('test', 'test')
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='127.0.1.1', credentials=credentials))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='live_data')
+
+    data = {
+        "domain": str(domain + "/shot.jpg"),
+        "lat" : lat,
+        "lng" : lng
+    }
+    message = json.dumps(data, ensure_ascii=False, indent=4)
+    
+    channel.basic_publish(exchange='', routing_key='live_data', body=message)
     print(" [x] Sent The JSON Data")
     connection.close()

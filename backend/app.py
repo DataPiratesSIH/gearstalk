1991c436e5e3eb2300ec6256c1e5dc4e39334e07
amurto <amurtobasu@gmail.com>
2020-04-25 21:59:38 +0530

    removed old routes

--- backend/app.py
+++ backend/app.py
@@ -37,47 +37,6 @@ app.register_blueprint(helpers, url_prefix="/helpers")
             merged-routes
 -----------------------------------'''
 
-@app.route('/')
-def index():
-    try:
-        return '''
-            <form method = "POST" action="/upload" enctype="multipart/form-data">
-                <input type="file" name='video'>
-                <input type="submit">
-            </form>
-        '''
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
-
-@app.route('/upload', methods=['POST'])
-def detect():
-    try:
-        file = request.files['video']
-
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            # create the folders when setting up your app
-            os.makedirs(os.path.join(app.instance_path, 'UPLOAD_FOLDER'), exist_ok=True)
-            file_path = os.path.join(app.instance_path, 'UPLOAD_FOLDER', filename)
-            print(file_path)
-            file.save(file_path)
-
-            # path = "C:\\Users\\Lenovo\\Downloads\\Documents\\GitHub\\yolo_textiles\\Object-detection\\videos\\airport.mp4"
-            vidcap = cv2.VideoCapture(file_path)
-            sec = 0
-            frameRate = 0.5                                              #it will capture image in each 0.5 second
-            success = getFrame(vidcap,sec,filename)
-            while success:
-                sec = sec + frameRate
-                sec = round(sec, 2)
-                success = getFrame(vidcap,sec,filename)
-        return jsonify({"success": file_path}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
-
-
 # @app.route('/processing/<video_id>', methods=['POST'])                    #if passing id through url
 # def process(video_id):
 
@@ -105,31 +64,6 @@ def process():
         return jsonify({"status": "Video will be processed in a while!!"}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
-
-
-@app.route("/register_cam", methods=['POST'])
-def register():
-    try:
-        req_data = json.loads(request.data)
-        web_url = req_data['url']
-        web_addr = req_data['addr']
-        location = geocode_address(web_addr)
-        web_lat = location[0]
-        web_lng = location[1]
-        print(web_lat,web_lng)
-
-        record = {
-                'url' : web_url,
-                'lat' : web_lat,
-                'lng' : web_lng,
-                'addr' : web_addr
-                }
-        db.cams.insert_one(record)
-        return jsonify({"status": "Record Sucessfully Added!!"}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
-
 
 @app.route('/livestream', methods=['GET'])
 def livestream():

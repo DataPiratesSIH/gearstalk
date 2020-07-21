02734d455fb2e94e6937270f661da91d0f9b3a8e
Shubham  Bhate <48563368+ssb2920@users.noreply.github.com>
2020-07-21 13:25:18 +0530

    Update FAQs.tsx

--- frontend/src/components/tools/FAQs.tsx
+++ frontend/src/components/tools/FAQs.tsx
@@ -9,37 +9,44 @@ import {
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-const data = [
-  {
-    title: "How many components can be identfied?",
-    content:
-      "Based on the video uploaded,components like shirts, pants, caps, sarees, blazers, burkhas, shorts, etc can be identified accurately ",
-  },
-  {
-    title: "What are the specifications for the size of the video?",
-    content:
-      "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
-  },
-  {
-    title: "How do I upload the video?",
-    content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem. 
-            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam. 
-            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat. 
-            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
-  },
-  {
-    title: "What are the technologies used in the making of this project?",
-    content: "v1.0.0",
-  },
-  {
-    title: "How do I find out a particular location for camera feed? ",
-    content: "v1.0.0",
-  },
-  {
-    title: "How do I access a particular camera from a location?",
-    content: "v1.0.0",
-  },
-];
+const data = {
+    title:"Frequently Asked Questions",
+    rows: [
+        {
+            title: "How many attributes can be identfied?",
+            content: "Based on the video uploaded,attributes like shirts,pants,caps,sarees,blazers,burkhas,shorts,etc can be identified accurately ",
+        },
+        {
+            title: "What are the specifications for the size of the video?",
+            content:
+                "There are no limitations regarding the size of the video, but in case faster processing is required it should be of atleast 30 sec duration.",
+        },
+        {
+            title: "How do I upload the video?",
+            content: `To add a new video, the user can click on Add video option. The timestamp is recorded and specific location can also be selected. `,
+        },
+        {
+            title: "What are the technologies used in the making of this project?",
+            content: "The diffent technologies used are Tensorflow, node, firebase, react, flask, mongo dB, grid fs",
+        },
+        {
+            title: "How do I find out a particular location for camera feed? ",
+            content: "In the Maps option, to search for a particular video feed or camera , one can navigate to any location using the map integrated using Mapbox API.",
+        },
+        {
+            title: "How do I find an already uploaded video?",
+            content: "All the previously uploaded videos are stored in the database and can be accessed by clicking the Library option in the side drawer.",
+        },
+        {
+            title: "What is the purpose of the Search option?",
+            content: "Search operation can be performed on the basis of various clothing attributes such as blazers, kurtas, long pants, Jersey, and so on. There is an option to search for more than one person in the video using the add person option. Multiple people can be added added or can be removed from the search. Another feature is that a search option can be performed through the description of a person. The user can just type the suspicious individuals description and based on this query the search can be performed.",
+        },
+        {
+            title: "How are the attributes identified once the video is uploaded?",
+            content: "Once a video is chosen processing is done on it. The video is broken into multiple frames and processing is done frame by frame. All the people in the frame along with their clothing attributes are identified and bases on the query the frame containing the appropriate person is returned.",
+        },
+    ],
+};
 
 const useStyles = makeStyles((theme) => ({
   heading: {
